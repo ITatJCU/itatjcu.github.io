f5c226975318143a40c04586897d4b5b509883bf
lindsaymarkward <lindsay.ward@jcu.edu.au>
2015-06-26 18:16:19 +1000

    Trevor's update to prizes - one button each

--- prizes/prizedraw.js
+++ prizes/prizedraw.js
@@ -20,8 +20,19 @@ function prizes() {
     outputWinners(chosenPeople, ids);
 }
 
+function singlePrize(city, id) {
+    var tickets = city == 'tsv' ? ticketstsv : ticketscns;
+    var choice = choose(1, tickets.length);
+    var chosenPeople = [];
+
+    chosenPeople.push(tickets[choice[0]][1] + " " + tickets[choice[0]][2]);
+
+    outputDots(chosenPeople, [id], '.');
+    // outputWinners(chosenPeople, [id]);
+}
+
 function choose(numberToChoose, maximum) {
-    var choices = []
+    var choices = [];
     var choice = Math.floor((Math.random() * maximum) + 1);
     choices.push(choice);
     for (var i = 1; i < numberToChoose; i++) {
@@ -41,3 +52,16 @@ function outputWinners(chosenPeople, ids) {
         prizeElement.innerHTML = chosenPeople[i];
     }
 }
+
+function outputDots(chosenPeople, id, dots) {
+    var prizeElement = document.getElementById(id[0]);
+    prizeElement.innerHTML = dots;
+
+    dots += '.';
+    if (dots != '....') {
+        window.setTimeout(outputDots, 500, chosenPeople, id, dots);
+    } else {
+        window.setTimeout(outputWinners, 500, chosenPeople, id);
+    }
+}
+
